41460858f58e21837f3c50af555f33771dc1c8d9
cameroncurry <cameroncurry@live.ca>
2014-12-28 13:52:22 +0000

    testing commit

---
+++ src/StockData.java
@@ -0,0 +1,15 @@
+/**
+ * @author Cameron Curry
+ *
+ *Stock data class holds arrays of raw data from yahoo
+ *Stock data is immutable for thread safety 
+ *
+ */
+
+public class StockData {
+
+	public StockData(){
+		
+		
+	}
+}
