925e40c057f197cddc514a70ca3a5fad19c669a1
cameroncurry <cameroncurry@live.ca>
2015-08-13 21:38:22 -0400

    eclipse files needed

--- src/TestStocks.java
+++ src/TestStocks.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Arrays;
 
 public class TestStocks {
 
 	public static void main(String[] args) {
+		/*
+		BTStock aapl = new BTStock("AAPL-0-1-2014-11-20-2014-m");
 		
-		BTStock aapl = new BTStock("AAPL-0-1-2014-11-20-2014-m",6);
-		
-		BTStock sp500 = new BTStock(Indexes.SP_500,"0-1-2014-11-20-2014-m",6);
+		BTStock sp500 = new BTStock(Indexes.SP_500,"0-1-2014-11-20-2014-m");
 		
 		sp500.data().print();
 		System.out.println();
 		aapl.data().print();
+		*/
 		
+		
+		List<String> list = new LinkedList<String>(Arrays.asList("2014-01-02", "2014-02-03", "2014-03-03",
+				"2014-04-01", "2014-05-01", "2014-06-02", "2014-07-01",
+				"2014-08-01", "2014-09-02", "2014-10-01", "2014-11-03",
+				"2014-12-01"));
+		
+		Iterator<String> iter = list.iterator();
+		while(iter.hasNext()){
+			iter.next();
+			iter.remove();
+			//System.out.println(iter.next());
+		}
+
 		/*
 		Sharpe s = new Sharpe(aapl);
 		
